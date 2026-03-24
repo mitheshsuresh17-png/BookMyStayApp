1ccbd560ce9f91031c635625723900b880ea6f7a
mitheshsuresh17-png <mitheshsuresh17@gmail.com>
2026-03-24 16:10:47 +0530

    UC1: Add welcome message module

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,20 +1,99 @@
-/**
- * Hotel Booking Application
- * This class represents the entry point of the Book My Stay application.
- * It prints a welcome message and version information.
- *
- * @author Mithesh
- * @version 1.0
- */
+// Version 2.1 - Refactored Implementation
 
-public class UseCase1HotelBookingApp {
+// Abstract Class
+abstract class Room {
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    // Constructor
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    // Abstract method
+    public abstract String getRoomType();
+
+    // Common method
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + getRoomType());
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+// Single Room Class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 200, 1500);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Single Room";
+    }
+}
+
+// Double Room Class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 350, 2500);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Double Room";
+    }
+}
+
+// Suite Room Class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 500, 5000);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Suite Room";
+    }
+}
+
+// Main Class
+public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("====================================");
-        System.out.println(" Welcome to Book My Stay Application ");
-        System.out.println(" Hotel Booking System v1.0 ");
-        System.out.println("====================================");
+        // Creating Room Objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
+        // Static Availability Variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display Details
+        System.out.println("===== HOTEL ROOM AVAILABILITY =====\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println("-----------------------------------");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println("-----------------------------------");
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+        System.out.println("-----------------------------------");
+
+        System.out.println("\nApplication Terminated.");
     }
 }
